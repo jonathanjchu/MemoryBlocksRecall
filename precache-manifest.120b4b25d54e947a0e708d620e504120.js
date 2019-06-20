55e41db96a2c9bfca0882c1488966bb47934e5d4
Jonathan Chu <jonathanjchu@gmail.com>
2019-06-20 03:06:28 -0400

    Updates

---
+++ precache-manifest.120b4b25d54e947a0e708d620e504120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3720b39a22a379b661203ddcf250d87a",
+    "url": "/MemoryBlocksRecall/index.html"
+  },
+  {
+    "revision": "36f2652f050587f2e4cd",
+    "url": "/MemoryBlocksRecall/static/css/main.2c9de409.chunk.css"
+  },
+  {
+    "revision": "d27dcead68faee6d2b37",
+    "url": "/MemoryBlocksRecall/static/js/2.44949046.chunk.js"
+  },
+  {
+    "revision": "36f2652f050587f2e4cd",
+    "url": "/MemoryBlocksRecall/static/js/main.b4e59e7a.chunk.js"
+  },
+  {
+    "revision": "ddaf694b524a6edd0d15",
+    "url": "/MemoryBlocksRecall/static/js/runtime~main.83e93243.js"
+  }
+]);
